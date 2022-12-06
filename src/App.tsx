35645ecfc8267bf52708d902cdbcec2c9639f293
Leonardo Dias <leonardodias97@gmail.com>
2022-12-06 17:55:09 -0300

    Links, catalog, product details

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ClientHome />}>
-        <Route index element={<Catalog />} />
+          <Route index element={<Catalog />} />
           <Route path="catalog" element={<Catalog />} />
-          <Route path="product-details" element={<ProductDeatils />} />
+          <Route path="product-details/:productId" element={<ProductDeatils />} />
         </Route>
       </Routes>
     </BrowserRouter>
